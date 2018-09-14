5a27fc28af753d30db5328697f58cde77b95882b
yurkevich-serj <yurkevichserj@gmail.com>
2018-09-14 16:58:35 +0300

    Added message to master again

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		System.out.println("Hello World from master branch!");
 		System.out.println("Hello World from dev branch!");
 		System.out.println("Hello World from feature branch!");
+		System.out.println("Hello World from master branch again!");
 	}
 }
